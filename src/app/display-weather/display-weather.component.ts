a79c48b1e24fd9d940393ec4526fa9b94ab66e84
Christian Christian <christianchristian1107@gmail.com>
2019-12-06 09:12:17 -0500

    2nd commit -chris

--- src/app/display-weather/display-weather.component.ts
+++ src/app/display-weather/display-weather.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { WeatherService } from "../services/weather.service";
 
 @Component({
-  selector: 'app-display-weather',
-  templateUrl: './display-weather.component.html',
-  styleUrls: ['./display-weather.component.css']
+  selector: "app-display-weather",
+  templateUrl: "./display-weather.component.html",
+  styleUrls: ["./display-weather.component.css"]
 })
 export class DisplayWeatherComponent implements OnInit {
+  weather: any[];
+  constructor(private weatherService: WeatherService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  searchGenre() {
+    this.weatherService.getWeather().subscribe(data => {
+      this.genres = data.genres;
+    });
   }
 
+  ngOnInit() {
+    this.getWeather();
+  }
 }
---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+const API_KEY = "f53eef0b095fada72ed938b843a44d7f";
+
+@Injectable({
+  providedIn: "root"
+})
+export class WeatherService {
+  constructor(private http: HttpClient) {}
+  getWeather(): Observable<any> {
+    return this.http.get("http://api.openweathermap.org/data/2.5/weather", {
+      params: {
+        api_key: API_KEY
+      }
+    });
+  }
+}
