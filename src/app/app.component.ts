f0367eba17731ed04f6e6ccc1d5107525490a016
Liam Deppong <liamdepp0605@gmail.com>
2019-12-17 11:03:07 -0500

    Fixed some stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { PackingListService } from './services/packing-list.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = "gearIt";
+
+  constructor(private packingListService: PackingListService){}
+
+  counter: number = 3;
+
+  subscription = this.packingListService.getMessage().subscribe(counter => {
+    // if (message === "add counter") {
+    //   this.counter++
+    // } else if (message === "subtract counter" && this.counter > 0) {
+    //   // clear messages when empty message received
+    //   this.counter--
+    // }
+    this.counter = counter;
+  });
+
+  NgOnChanges(){
+  }
+
+  ngOnInit(): void {
+  }
+
 }
+// This.subject.next
